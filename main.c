2f1169c08dfb838cac82d256cfed242e59c36e34
Alvi2023 <136925197+Alvi2023@users.noreply.github.com>
2023-06-20 00:23:48 +0600

    added login pannel

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "splash.h"
+#include "login.h"
 
 int main()
 {
     recursive(0); //use of recursion
     splash();
+    system("cls");
+    login();
     return 0;
 }
